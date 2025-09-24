30ce3711c0b2e2e887df98b215cb34285b28a5fb
Valen178 <valentino.cortese@davinci.edu.ar>
2025-09-24 09:34:30 -0300

    Add logo and title to the tab

--- src/pages/Home/components/UserHome.jsx
+++ src/pages/Home/components/UserHome.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export function UserHome() {
   return (
     <div className='min-h-screen m-4'>
-        <h1 className='text-2xl font-bold text-black dark:text-white'>Soy Home de User</h1>
+        <h1 className='text-2xl font-bold text-black dark:text-white'>Soy Home</h1>
     </div>
   );
 }
